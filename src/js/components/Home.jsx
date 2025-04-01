3c6976e22e36dd15f00558c0fe869ec921706d8a
Neil-Player456 <Nmrubix11@gmail.com>
2025-04-01 23:14:16 +0000

    add async func to add task

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
 
@@ -7,26 +7,48 @@ import { useState } from "react";
 
 
 //create your first component
-const Home = () => { 
+const Home = () => {
 	const [tasks, setTasks] = useState([]);
 	const [newTask, setNewTask] = useState("")
-
-	
-	function AddTask (e) { 
-		const taskList = tasks
-	
-
-		if(newTask.trim() !== "") {
-			setTasks([...taskList, newTask])
-			setNewTask("")
+	useEffect(() => {
+		const fetchData = async () => {
+			const responce = await fetch("https://playground.4geeks.com/todo/users/nmorris11")
+			if (!responce.ok) {
+				alert("error fetching data")
+			}
+			const result = await responce.json()
+			setTasks(result.todos.map(task => task.label))
 		}
+		fetchData()
 
-	}
+	}, [])
 
-	function DeleteTask (index) {
+
+	// function AddTask(e) {
+	// 	const taskList = tasks
+
+
+	// 	if (newTask.trim() !== "") {
+	// 		setTasks([...taskList, newTask])
+	// 		setNewTask("")
+	// 	}
+
+	// }
+
+	async function AddTask(e) {
+			const response = await fetch("https://playground.4geeks.com/todo/todos/nmorris11", {
+			  method: "POST",
+			  headers: {
+				"Content-Type": "application/json",
+			  },
+			  body: JSON.stringify({ label: newTask }), 
+			});
 	
+
+	function DeleteTask(index) {
+
 		setTasks(tasks.filter((task, i) => index !== i))
-		
+
 	}
 
 
@@ -35,27 +57,35 @@ const Home = () => {
 	return (
 		<div className="Container">
 			<h1 className="Title">Gotta Do It When It Gotta Get Done</h1>
-			
+
 			<div className="ToDo-Bar">
-				<input type="Text" className="Entered-list" placeholder="Enter Task Then Tap Dat..." value ={newTask} onChange={e => setNewTask(e.target.value)}></input>
+				<input type="Text" className="Entered-list" placeholder="Enter Task Then Tap Dat..." value={newTask} onChange={e => setNewTask(e.target.value)}></input>
 				<button onClick={() => AddTask()} className="Add-List">Tap Dat</button>
 			</div>
 
 			<div className="Box-List">
+
 				<ul>
-					{tasks.map((task,index) => (
-					<li className="d-flex justify-content-between" key={index}>
-						{task}
-						<button onClick={() => DeleteTask(index)} className="Delete-Button">XXXX</button>
-					</li>	
-					))}
+					{tasks.length > 0 ? (
+
+						tasks.map((task, index) => (
+							<li className="d-flex justify-content-between" key={index}>
+								{task}
+								<button onClick={() => DeleteTask(index)} className="Delete-Button">XXXX</button>
+							</li>
+						))
+
+					) : (
+						<li>no task available</li>
+					)}
+
 				</ul>
-				
-				
-				
-				
-				
-				
+
+
+
+
+
+
 			</div>
 
 
